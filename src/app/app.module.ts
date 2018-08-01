38919447b035b0d6bc2d8235f6eb1fff411c0c52
Gustavo Contreras <qwertygussaints@gmail.com>
2018-08-01 11:54:18 -0600

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+
+/* rutas */
+import { APP_ROUTES } from './app.routes';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    SidebarComponent,
+    ProgressComponent,
+    DashboardComponent,
+    Graficas1Component,
+    BreadcrumbsComponent,
+    NopagefoundComponent,
+    PagesComponent,
+    RegisterComponent,
   ],
   imports: [
+    APP_ROUTES,
     BrowserModule
   ],
   providers: [],
