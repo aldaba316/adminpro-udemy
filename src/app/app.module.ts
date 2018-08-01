551e23e439096455d60b7acc42e715dbbf9576a5
Gustavo Contreras <qwertygussaints@gmail.com>
2018-08-01 12:56:50 -0600

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,35 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-
-/* rutas */
-import { APP_ROUTES } from './app.routes';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+
+/* Modules */
+import { PagesModule } from './pages/pages.module';
+
+/* routes */
+import { APP_ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HeaderComponent,
-    SidebarComponent,
-    ProgressComponent,
-    DashboardComponent,
-    Graficas1Component,
-    BreadcrumbsComponent,
-    NopagefoundComponent,
-    PagesComponent,
     RegisterComponent,
   ],
   imports: [
     APP_ROUTES,
+    PagesModule,
     BrowserModule
   ],
   providers: [],
